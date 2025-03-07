1d63adb24233e2f38e77ace6bc0926250f068190
sajeebKarmaker <1810052@student.ruet.ac.bd>
2025-03-07 16:03:35 +0600

    Cart feature added

--- E-commerce-app/src/app/app.module.ts
+++ E-commerce-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -15,6 +17,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
@@ -22,7 +26,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ShippingComponent,
+    CartComponent,
   ],
   bootstrap: [
     AppComponent
